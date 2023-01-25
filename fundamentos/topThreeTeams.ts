334776a98584c9d33b92f1d55d54ccf740755e7f
Jesse Alves <jessecarlos@gmail.com>
2023-01-24 21:38:51 -0300

    Deixando dados fixos

--- fundamentos/topThreeTeams.ts
+++ fundamentos/topThreeTeams.ts
@@ -2,7 +2,8 @@ import {Result, results} from "./data/results";
 
 //Somar a pontuacao por equipe e retornar as 3 melhores equipes
 const topThreeTeams = (results: Result[]) => {
-    return 0;
+    return [];
 }
 
+// resultado esperado = [{team: "Red Bull", points: 50}, { team: "Mercedes", points: 45}, { team: McLaren, points: 38 }]
 console.log(topThreeTeams(results));
