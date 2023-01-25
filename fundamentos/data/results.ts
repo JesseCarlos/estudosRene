1c93cd22a8cc647d18636eb69056b0d05bf4d553
Jesse Alves <jessecarlos@gmail.com>
2023-01-24 21:37:15 -0300

    Deixando dados fixos

--- fundamentos/data/results.ts
+++ fundamentos/data/results.ts
@@ -1,13 +1,93 @@
-import {faker} from "@faker-js/faker";
-
 export interface Result {
     driver: string;
     team: string;
     points: number;
 }
 
-export const results: Result[] = Array(Number(20)).fill('').map(() => ({
-    driver: faker.name.firstName(),
-    team: faker.helpers.arrayElement(["Mercedes", "Ferrari", "Red Bull", "McLaren", "Renault"]),
-    points: faker.helpers.arrayElement([25, 18, 15, 12, 10, 8, 6, 4, 2, 1])
-}));
+export const results: Result[] = [
+    {
+        driver: "Lewis Hamilton",
+        team: "Mercedes",
+        points: 20
+    },
+    {
+        driver: "Lewis Hamilton",
+        team: "Mercedes",
+        points: 8
+    },
+    {
+        driver: "Lewis Hamilton",
+        team: "Mercedes",
+        points: 2
+    },
+    {
+        driver: "Valtteri Bottas",
+        team: "Mercedes",
+        points: 10
+    },
+    {
+        driver: "Valtteri Bottas",
+        team: "Mercedes",
+        points: 5
+    },
+    {
+        driver: "Charles Leclerc",
+        team: "Ferrari",
+        points: 7
+    },
+    {
+        driver: "Sebastian Vettel",
+        team: "Ferrari",
+        points: 3
+    },
+    {
+        driver: "Charles Leclerc",
+        team: "Ferrari",
+        points: 8
+    },
+    {
+        driver: "Sebastian Vettel",
+        team: "Ferrari",
+        points: 10
+    },
+    {
+        driver: "Max Verstappen",
+        team: "Red Bull",
+        points: 5
+    },
+    {
+        driver: "Sergio Perez",
+        team: "Red Bull",
+        points: 10
+    },
+    {
+        driver: "Max Verstappen",
+        team: "Red Bull",
+        points: 20
+    },
+    {
+        driver: "Sergio Perez",
+        team: "Red Bull",
+        points: 15
+    },
+    {
+        driver: "Lando Norris",
+        team: "McLaren",
+        points: 10
+    },
+    {
+        driver: "Carlos Sainz",
+        team: "McLaren",
+        points: 5
+    },
+    {
+        driver: "Lando Norris",
+        team: "McLaren",
+        points: 15
+    },
+    {
+        driver: "Carlos Sainz",
+        team: "McLaren",
+        points: 8
+    }
+];
--- fundamentos/teamRanking.ts
+++ fundamentos/teamRanking.ts
@@ -2,7 +2,9 @@ import {Result, results} from "./data/results";
 
 //Somar os pontos por equipe e ordenar por ordem crescente de pontuação
 const rankedTeams = (results: Result[]) => {
-    return 0;
+    return [];
 }
+
+// resultado esperado = [{team: "Red Bull", points: 50}, { team: "Mercedes", points: 45}, { team: McLaren, points: 38 }, { team: "Ferrari", points: 28}]
 
 console.log(rankedTeams(results));
