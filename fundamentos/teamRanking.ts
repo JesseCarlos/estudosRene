071b1cdfe33cfdbc57efe093212863577d86f6f0
Jesse Alves <jessecarlos@gmail.com>
2023-01-29 22:27:12 -0300

    Deixando dados fixos

--- fundamentos/teamRanking.ts
+++ fundamentos/teamRanking.ts
@@ -1,10 +1,30 @@
 import {Result, results} from "./data/results";
 
 //Somar os pontos por equipe e ordenar por ordem crescente de pontuação
+
+interface TeamResult {
+    team: string;
+    points: number;
+}
+
 const rankedTeams = (results: Result[]) => {
-    return [];
+    const teamResults: TeamResult[] = []
+
+    for (let i = 0; i < results.length; i++) {
+        let info = results[i];
+        let time = info.team;
+
+        teamResults.push({
+            team: info.team,
+            points: info.points
+        })
+    }
+
+    return teamResults;
 }
 
 // resultado esperado = [{team: "Red Bull", points: 50}, { team: "Mercedes", points: 45}, { team: McLaren, points: 38 }, { team: "Ferrari", points: 28}]
 
-console.log(rankedTeams(results));
+const teamResults = rankedTeams(results);
+
+console.log(teamResults)
